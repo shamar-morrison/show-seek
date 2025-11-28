694a98884290535d031c816c48369a5228bd6094
Shamar Morrison <shamar.webdev@gmail.com>
2025-11-27 22:41:54 -0500

    style: Adjust list content spacing by replacing paddingBottom with marginLeft.

--- app/(tabs)/library/index.tsx
+++ app/(tabs)/library/index.tsx
@@ -224,7 +224,7 @@ const styles = StyleSheet.create({
   },
   listContent: {
     paddingHorizontal: SPACING.l,
-    paddingBottom: 100,
+    marginLeft: SPACING.s,
   },
   mediaCard: {
     width: ITEM_WIDTH,
