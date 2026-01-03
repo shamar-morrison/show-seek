6ed65375cf4fc869877562bab8da7592d8e80dcf
Shamar Morrison <shamar.webdev@gmail.com>
2026-01-03 16:56:13 -0500

    feat(native): implement SharedDataModule and register package

---
+++ android/app/src/main/java/app/horizon/showseek/SharedDataPackage.kt
@@ -0,0 +1,19 @@
+package app.horizon.showseek
+
+import android.view.View
+import com.facebook.react.ReactPackage
+import com.facebook.react.bridge.NativeModule
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.uimanager.ReactShadowNode
+import com.facebook.react.uimanager.ViewManager
+
+class SharedDataPackage : ReactPackage {
+
+    override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> {
+        return listOf(SharedDataModule(reactContext))
+    }
+
+    override fun createViewManagers(reactContext: ReactApplicationContext): List<ViewManager<*, *>> {
+        return emptyList()
+    }
+}
