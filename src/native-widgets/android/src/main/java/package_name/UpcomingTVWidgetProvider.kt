5007d55ce26362149c0cabead3cc2a613e6270bb
Shamar Morrison <shamar.webdev@gmail.com>
2026-09-10 23:38:27 -0500

    fix(widgets): restore BitmapFactory imports stripped in intent rework

--- src/native-widgets/android/src/main/java/package_name/UpcomingTVWidgetProvider.kt
+++ src/native-widgets/android/src/main/java/package_name/UpcomingTVWidgetProvider.kt
@@ -6,6 +6,7 @@ import android.appwidget.AppWidgetProvider
 import android.content.Context
 import android.content.Intent
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.view.View
 import android.widget.RemoteViews
 import org.json.JSONArray
